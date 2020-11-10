2551b237310b081407269797638172bee041d40c
Gustizain <74238476+Gustizain@users.noreply.github.com>
2020-11-10 19:55:12 +0700

    Percabangan

---
+++ Percabangan/src/percabangan/IfThen.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percabangan;
+
+public class IfThen {
+
+    public static void main(String[] args) {
+  boolean isOn = true;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        }
+    }
+    
+}
