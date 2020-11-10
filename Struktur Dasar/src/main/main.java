c8c69c39f6678faaeb5ac15c5a41534cf3f77663
Gustizain <74238476+Gustizain@users.noreply.github.com>
2020-11-10 19:56:14 +0700

    Struktur Data

---
+++ Struktur Dasar/src/main/main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import musik.gitar;
+import kendaraan.kereta;
+import kendaraan.mobil;
+import kendaraan.motor;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+        gitar.bunyi();
+        kereta.jumlahBan();
+        mobil.jumlahBan();
+        motor.jumlahBan();
+    }
+    
+}
---
+++ Struktur Dasar/src/musik/gitar.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musik;
+
+public class gitar {
+   public static void bunyi(){
+        System.out.println("jrenggg..");
+    } 
+}
