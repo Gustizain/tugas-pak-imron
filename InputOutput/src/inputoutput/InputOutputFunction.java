10ff0ec6878d207336263bf6ab7e3779058de21c
Gustizain <74238476+Gustizain@users.noreply.github.com>
2020-11-10 19:52:41 +0700

    InputOutput

---
+++ InputOutput/src/inputoutput/InputOutputFunction.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inputoutput;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputOutputFunction {
+
+    
+  
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Program penjumalahan sangat sederhana");
+        System.out.print("Masukan Angka pertama : ");
+        int value = scanner.nextInt();
+        System.out.print("Masukan Angka kedua : ");
+        int anotherValue = scanner.nextInt();
+        int result = value + anotherValue;
+        System.out.println("Hasilnya adalah : " + result); 
+    }
+    
+}
